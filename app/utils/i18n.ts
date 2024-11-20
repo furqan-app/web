07f4b641dc5a83f418f89e8e604bda5d7a0d3766
usef <usefrady@gmail.com>
2024-11-20 22:55:21 +0200

    Remove lodash

--- app/utils/i18n.ts
+++ app/utils/i18n.ts
@@ -1,12 +1,18 @@
-import _ from 'lodash';
-
 const RTL_LANGUAGES = ['ar'];
 
 export function convertFlatToNested(flatMessages: Record<string, string>): Record<string, any> {
   return Object.entries(flatMessages).reduce((acc, [key, value]) => {
-    _.set(acc, key, value);
+    const keys = key.split('.');
+    let current = acc;
+    
+    for (let i = 0; i < keys.length - 1; i++) {
+      current[keys[i]] = current[keys[i]] || {};
+      current = current[keys[i]];
+    }
+    
+    current[keys[keys.length - 1]] = value;
     return acc;
-  }, {});
+  }, {} as Record<string, any>);
 }
 
 export function getLanguageDirection(language: string) {
