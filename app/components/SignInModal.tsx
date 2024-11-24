fc20210a4e90dd165e86f334db2ec02415992cae
usef <usefrady@gmail.com>
2024-11-24 21:53:10 +0200

    Fix signin modal

--- app/components/SignInModal.tsx
+++ app/components/SignInModal.tsx
@@ -9,23 +9,21 @@ export type SignInModalProps = {
 export const SignInModal = ({ close, isOpen }: SignInModalProps) => {
   return (
     <FQModal isOpen={isOpen} close={close}>
-      {({ close }) => (
-        <FQModal.Body close={close}>
-          <div className="flex flex-col gap-4">
-            <h1 className="text-xl font-semibold text-black dark:text-white">
-              Sign in first to start marking.
-            </h1>
-            <div>
-              <button
-                className="inline-flex items-center gap-2 rounded-md bg-green-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none hover:bg-green-600 focus:outline-1 focus:outline-white"
-                onClick={() => signIn()}
-              >
-                Sign in
-              </button>
-            </div>
+      <FQModal.Body close={close}>
+        <div className="flex flex-col gap-4">
+          <h1 className="text-xl font-semibold text-black dark:text-white">
+            Sign in first to start marking.
+          </h1>
+          <div>
+            <button
+              className="inline-flex items-center gap-2 rounded-md bg-green-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none hover:bg-green-600 focus:outline-1 focus:outline-white"
+              onClick={() => signIn()}
+            >
+              Sign in
+            </button>
           </div>
-        </FQModal.Body>
-      )}
+        </div>
+      </FQModal.Body>
     </FQModal>
   );
 };
